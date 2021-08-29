4dff1e5deced615f83f2d58c4b8c20ed54db3566
Jayesh Mali <68943760+TheJayesh99@users.noreply.github.com>
2021-08-29 11:21:09 +0530

    Added : Method to create contact in address book

--- AddressBookMain.py
+++ AddressBookMain.py
@@ -1,1 +1,7 @@
-print("Welcome to Address Book Management System")
+from AddressBookConsoleService import AddressBookConsoleService
+
+if __name__ == "__main__":
+    console_service = AddressBookConsoleService()
+    print("Welcome to Address Book Management System")
+    contact = console_service.createContact()
+    console_service.displayContact(contact)
